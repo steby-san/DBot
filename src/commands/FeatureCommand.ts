396cf6d85cfa477a1aaaa537c4e761ec1becdfac
steby-san <sanaeesan12@gmail.com>
2025-05-08 09:35:07 +0700

    thêm ship command :dorodoro:

--- src/commands/FeatureCommand.ts
+++ src/commands/FeatureCommand.ts
@@ -89,9 +89,42 @@ const GetPingCommand: Command = {
   }
 };
 
+// Shiping :')
+const ShipCommand: Command = {
+  data: new SlashCommandBuilder()
+    .setName('ship')
+    .setDescription('Ship 2 người và kiểm tra độ tương thích')
+    .addUserOption(option =>
+      option.setName('user1')
+        .setDescription('Người dùng 1')
+        .setRequired(true))
+    .addUserOption(option =>
+      option.setName('user2')
+        .setDescription('Người dùng 2')
+        .setRequired(true)),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user1 = interaction.options.getUser('user1');
+    const user2 = interaction.options.getUser('user2');
+    
+    if (!user1 || !user2) {
+      await interaction.reply('Vui lòng nhập tên người dùng');
+      return;
+    }
+    
+    const compatibility = Math.floor(Math.random() * 100) + 1;
+    const embed = {
+      color: 0x0099ff,
+      title: `${user1.username} và ${user2.username}`,
+      description: `Độ tương thích: ${compatibility}%`,
+      image: {
+        url: `https://i.pinimg.com/originals/86/00/bb/8600bb6e97e5043094c8bc4e643fcc0d.gif`
+      }
+    };
+    await interaction.reply({ embeds: [embed] });
+  }
+};
 
-
-const commandModules = [GetUserAvatarCommand, RollDiceCommand, FlipCoinCommand, GetPingCommand];
+const commandModules = [GetUserAvatarCommand, RollDiceCommand, FlipCoinCommand, GetPingCommand, ShipCommand];
 
 const featureCommands = new Collection<string, Command>();
 
